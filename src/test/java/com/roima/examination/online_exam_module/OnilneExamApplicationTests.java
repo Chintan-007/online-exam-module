73ad4677d574d80cace0a21b3ed9611c36a8580a
Chintan Gelot <chintan3701@gmail.com>
2024-11-10 01:09:22 +0530

    Initial commit: Set up project Structure with application.properties and dependencies

---
+++ src/test/java/com/roima/examination/online_exam_module/OnilneExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roima.examination.online_exam_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnilneExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
