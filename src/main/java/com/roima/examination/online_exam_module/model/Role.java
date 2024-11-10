5e8733e868802efd5fd444a5d1a0a596df6abf90
Chintan Gelot <chintan3701@gmail.com>
2024-11-11 00:25:08 +0530

    feat: initialize project package structure and add User Entity

---
+++ src/main/java/com/roima/examination/online_exam_module/model/Role.java
@@ -0,0 +1,6 @@
+package com.roima.examination.online_exam_module.model;
+
+public enum Role {
+	ADMIN,
+	STUDENT
+}
--- src/main/java/com/roima/examination/online_exam_module/model/User.java
+++ src/main/java/com/roima/examination/online_exam_module/model/User.java
@@ -1,5 +1,61 @@
 package com.roima.examination.online_exam_module.model;
 
-public class User {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+@Inheritance(strategy = InheritanceType.JOINED) //both 'Student' and 'Admin' will inherit from User
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String firstName;
+    
+    @Column(nullable = false)
+    private String lastName;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+    
+    @Column(nullable = false)
+    private boolean active = true;
+    
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    
+    @Column
+    private LocalDateTime lastLogin;
+    
+    @PrePersist //method that should be executed before an entity is persisted (i.e., saved) to the database for the first time.
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
